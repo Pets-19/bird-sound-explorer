544fe3e75d23cd52231dfc64e933925f1660abbc
Muhammad Sahl <wildwonderspawsomepals@gmail.com>
2025-05-05 13:09:48 +0530

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import ExplorePage from './pages/ExplorePage';
-import ModePage from './pages/ModePage';
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ExplorePage from "./pages/ExplorePage";
+import ModePage from "./pages/ModePage";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/explore" element={<ExplorePage />} />
-        <Route path="/explore/:modeId" element={<ModePage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/explore" element={<ExplorePage />} />
+				<Route path="/explore/:modeId" element={<ModePage />} />
+				<Route path="*" element={<Navigate to="/" replace />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
